5df7c9540b2e6156163964e8ed2c730af9b35608
Jungle-by <Jungle-by@users.noreply.github.com>
2026-08-01 11:35:09 +0800

    fix: fork repoint + v4 nftables fixes + deps to fork (chnroute/lkl-haproxy), drop accxio mirror
    - v4 fixes: SET_V2NODE guard, startup.sh, network-online unit, ui-NodeSave in Archive.zip
    - all download URLs -> Jungle-by/de_GWD (client/server/autoUpdate/version.php/ui)
    - deps: chnroute + lkl-haproxy -> Jungle-by forks; stl-thumb/Loyalsoldier stay upstream
    - remove author accxio mirror fallback (unmaintained risk)
    - README install cmds -> fork

--- resource/client/ui-web/index.php
+++ resource/client/ui-web/index.php
@@ -1043,7 +1043,7 @@ $.get('./act/markThis.php', {markName:markNametxt}, function(result){window.loca
 })
 
 $('#currentver').click(function(){
-window.open("https://github.com/jacyl4/de_GWD/releases")
+window.open("https://github.com/Jungle-by/de_GWD/releases")
 })
 
 $('#remotever').click(function(){
